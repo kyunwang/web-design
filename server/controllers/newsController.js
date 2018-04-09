c1bb357b966aed692ff664ea4e37b7fa2b62f9fb
Kangyun Wang <wky@013.184.028.145.hva.nl>
2018-04-09 16:54:56 +0200

    Made the apicall a bare macro for listitems and displayed

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,5 +1,25 @@
-import fetch from 'node-fetch';
+require('dotenv').config({ path: './vars.env' });
+
+import NewsApi from 'newsapi';
+const newsapi = new NewsApi(process.env.API_KEY);
+
+exports.getNews = async (req, res, next) => {
+	// return next();
+
+	req.headline = await newsapi.v2.topHeadlines({
+		// sources: 'bbc-news,the-verge',
+		// q: 'iphone',
+		category: 'technology',
+		language: 'en',
+		country: 'us'
+	});
+
+	next();
+}
 
 exports.homePage = (req, res) => {
-	res.render('homePage', { message: 'Hello Server' });
+	res.render('homePage', {
+		message: 'Hello Server',
+		headlines: req.headline && req.headline
+	});
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,10 @@ import newsController from '../controllers/newsController';
 
 const router = express.Router();
 
-router.get('/', newsController.homePage);
+// Need to wrap in a error handler
+router.get('/',
+	newsController.getNews,
+	newsController.homePage
+);
 
 export default router;
