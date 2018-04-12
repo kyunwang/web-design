3d8c2d0d2d7bee7e9bdd9246725f83b8a93f2d3e
Kangyun Wang <wky@kangyuns-mbp.home>
2018-04-12 20:03:51 +0200

    Update the readme

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Simple news app (Quick prototype)
-A simple news app prototype using NewsApi for minor class Web Design
+A simple news app prototype using NewsApi for minor class Web Design. 
+With as goal to implement atleast three principles from this [article][article]
 
+**Notes**
+- *This is a prototype to implement a few design principles and does not have the goal to be production ready*
+- *It is currently made for smaller screens*
+
+# Table of Content
+- [Getting started](#getting-started)
+- [The Principles](#the-principles)
+- [Features](#features)
+- [Whats used](#whats-used)
+- [To Do](#to-do)
+
+# Getting started
+Here is how to get started:
+1. Download or clone the repo `git clone https://github.com/kyunwang/web-design.git`
+2. Run `npm install` or `yarn`
+3. Do a `npm start`
+4. Go to [localhost:3200](localhost:3200) and you are ready to go.
+
+# The Principles
+- Strong visual hierarchies work best
+- Progressive disclosure
+- A crucial moment: the zero state
+- One primary action per screen
+
+# Features
+
+
+# To Do
+- [ ] Link the intro screen choices to the server
+- [ ] Fix the snapping in the country selection screen
+
+
+[article]: http://bokardo.com/principles-of-user-interface-design/
--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -1,50 +1,51 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-exports.$ = function(element) {
+"use strict";
+
+exports.$ = function (element) {
 	return document.querySelector(element);
 };
 
-exports.$$ = function(element) {
+exports.$$ = function (element) {
 	return document.querySelectorAll(element);
 };
+
 },{}],2:[function(require,module,exports){
+'use strict';
+
 const h = require('./helpers');
 
 const $ = h.$;
 const $$ = h.$$;
 
-(function() {
+(function () {
 	const bookmark = {
 		bookmarkLink: '',
-		init: function() {
+		init: function () {
 			this.bookmarkLink = $('#bookmark-link');
 
 			// this.bookmarkLink.addEventListener('click', this.updateBookmark);
 
 
 			console.log(this.bookmarkLink);
-			
+
 			const bookmarks = $$('.bookmark').forEach(this.addBookmark);
 		},
-		updateBookmark: function(el) {
+		updateBookmark: function (el) {
 			// el.preventDefault();
 			console.log('update');
-
-			
 		},
-		addBookmark: function(el) {
+		addBookmark: function (el) {
 			el.addEventListener('click', elem => {
-				const target =  elem.target;
-				
+				const target = elem.target;
+
 				fetch('/bookmarks', {
 					method: 'POST',
 					body: JSON.stringify({
 						id: target.id
 					}),
-					headers: {"Content-Type": "application/json"},
+					headers: { "Content-Type": "application/json" },
 					credentials: 'same-origin' // or 'include'
-				})
-				.then(resp => resp.json())
-				.then(data => {
+				}).then(resp => resp.json()).then(data => {
 					console.log(data);
 					el.classList.toggle('bookmark--marked');
 					bookmark.bookmarkLink.textContent = data.bookmarks.length;
@@ -52,11 +53,11 @@ const $$ = h.$$;
 
 				// console.log('bookmark', el.target.id);
 				// console.log(e.getAttribute('id'));
-
 			});
 		}
-	}
+	};
 
 	bookmark.init();
-})()
+})();
+
 },{"./helpers":1}]},{},[2]);
