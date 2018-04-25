d7629f41524eba5b98bd312e13c433a4fd52637d
Kangyun Wang <wky@Kangyuns-MacBook-Pro.local>
2018-04-25 15:46:26 +0200

    Shorten text

--- minor-website/dist/bundle.js
+++ minor-website/dist/bundle.js
@@ -1,1 +1,108 @@
-!function s(r,a,c){function l(e,t){if(!a[e]){if(!r[e]){var i="function"==typeof require&&require;if(!t&&i)return i(e,!0);if(d)return d(e,!0);var o=new Error("Cannot find module '"+e+"'");throw o.code="MODULE_NOT_FOUND",o}var n=a[e]={exports:{}};r[e][0].call(n.exports,function(t){return l(r[e][1][t]||t)},n,n.exports,s,r,a,c)}return a[e].exports}for(var d="function"==typeof require&&require,t=0;t<c.length;t++)l(c[t]);return l}({1:[function(t,e,i){"use strict";!function(){function t(t,e,i){t.forEach(function(t){t.addEventListener(e,i)})}var i={radioToggle:[],testSection:[],init:function(){var e=this;this.radioToggle=document.querySelectorAll('.testimonials__container [type="radio"]'),this.testSection=document.querySelectorAll(".testimonials__container li"),t(this.radioToggle,"click",function(t){return e.toggleSection(t)}),this.testSection.forEach(function(t,e){0!==e&&(t.classList.add("hide-testimonials"),t.classList.add("hide"))})},toggleSection:function(e){i.testSection.forEach(function(t){if(t.dataset.testimonial===e.target.id)return t.classList.remove("hide-testimonials"),t.classList.add("show-testimonials"),void setTimeout(function(){t.classList.remove("hide")},300);t.classList.remove("show-testimonials"),t.classList.add("hide-testimonials"),setTimeout(function(){t.classList.add("hide")},300)})}},o={moreToggle:[],moreContainer:[],init:function(){var e=this;console.log("Init toggle"),this.moreToggle=document.querySelectorAll(".button__more"),this.moreContainer=document.querySelectorAll(".more-info__container"),t(this.moreToggle,"click",function(t){return e.toggleDetail(t)}),this.moreContainer.forEach(function(t,e){t.classList.add("hide-more-info"),t.classList.add("hide")})},toggleDetail:function(e){var i=e.target.firstElementChild;o.moreContainer.forEach(function(t){if(t.dataset.moreInfo===e.target.id){if(t.classList.contains("hide"))return i.textContent="Minder",console.log("show"),t.classList.remove("hide-more-info"),void setTimeout(function(){t.classList.remove("hide"),t.classList.add("show-more-info")},300);i.textContent="Meer",t.classList.remove("show-more-info"),t.classList.add("hide-more-info"),setTimeout(function(){t.classList.add("hide")},300)}})}};i.init(),o.init()}()},{}]},{},[1]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+'use strict';
+
+(function () {
+	function addEvents(elements, event, callback) {
+		elements.forEach(function (el) {
+			el.addEventListener(event, callback);
+		});
+	}
+
+	var testimonials = {
+		radioToggle: [],
+		testSection: [], // As in testimonial section
+		init: function init() {
+			var _this = this;
+
+			this.radioToggle = document.querySelectorAll('.testimonials__container [type="radio"]');
+			this.testSection = document.querySelectorAll('.testimonials__container li');
+
+			// Add toggle evvents to the radiobuttons/labels
+			addEvents(this.radioToggle, 'click', function (evt) {
+				return _this.toggleSection(evt);
+			}, true);
+
+			// Hide all testimonials except first one if there is js
+			this.testSection.forEach(function (el, index) {
+				if (index === 0) return;
+				el.classList.add('hide-testimonials');
+				el.classList.add('hide');
+			});
+		},
+		toggleSection: function toggleSection(evt) {
+			// evt.preventDefault();
+			// Toggle the testimonial sections
+			testimonials.testSection.forEach(function (el) {
+				if (el.dataset.testimonial === evt.target.id) {
+					el.classList.remove('hide-testimonials');
+					el.classList.add('show-testimonials');
+					setTimeout(function () {
+						el.classList.remove('hide');
+					}, 300);
+					return;
+				}
+
+				el.classList.remove('show-testimonials');
+				el.classList.add('hide-testimonials');
+				setTimeout(function () {
+					el.classList.add('hide');
+				}, 300);
+			});
+		}
+	};
+
+	// For the meer info buttons
+	var sectionDetails = {
+		moreToggle: [],
+		moreContainer: [],
+		init: function init() {
+			var _this2 = this;
+
+			console.log('Init toggle');
+			this.moreToggle = document.querySelectorAll('.button__more');
+			this.moreContainer = document.querySelectorAll('.more-info__container');
+
+			// Add toggle evvents to the radiobuttons/labels
+			addEvents(this.moreToggle, 'click', function (evt) {
+				return _this2.toggleDetail(evt);
+			}, true);
+
+			// this.moreToggle.forEach(el => {});
+			// Hide all testimonials except first one if there is js
+			this.moreContainer.forEach(function (el, index) {
+				el.classList.add('hide-more-info');
+				el.classList.add('hide');
+			});
+		},
+		toggleDetail: function toggleDetail(evt) {
+			// console.log(evt.target.firstElementChild);
+			var buttonText = evt.target.firstElementChild;
+			sectionDetails.moreContainer.forEach(function (el) {
+				if (el.dataset.moreInfo === evt.target.id) {
+					if (el.classList.contains('hide')) {
+						buttonText.textContent = 'Minder';
+						console.log('show');
+						el.classList.remove('hide-more-info');
+						setTimeout(function () {
+							el.classList.remove('hide');
+							el.classList.add('show-more-info');
+						}, 300);
+						return;
+					} else {
+						buttonText.textContent = 'Meer';
+						el.classList.remove('show-more-info');
+						el.classList.add('hide-more-info');
+						setTimeout(function () {
+							el.classList.add('hide');
+						}, 300);
+					}
+				}
+			});
+		}
+	};
+
+	testimonials.init();
+	sectionDetails.init();
+})();
+
+},{}]},{},[1]);
